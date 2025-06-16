07e6e80bd02251c95bff670fbb3918f468d46b4b
orestulis05 <orestaspetkus@gmail.com>
2025-06-16 09:55:10 +0300

    init

---
+++ Agent/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO.Pipes;
+
+// Get directory path from args
+
+// Connection to the master process
+using var client = new NamedPipeClientStream(".", "mypipe", PipeDirection.Out);
+Console.WriteLine("Connecting to Master process...");
+client.Connect();
+Console.WriteLine("Connected!");
+
+// Get files from a directory
+// Read through every file
+// Send data to the Master process
+
+using var writer = new StreamWriter(client) { AutoFlush = true };
+writer.WriteLine("Hello from the agent!");
+
+// writer.Flush();
+
+Console.ReadKey();
---
+++ Master/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.IO.Pipes;
+using System.Text;
+
+// Connection to agent process
+using var server = new NamedPipeServerStream("mypipe", PipeDirection.In);
+
+try
+{
+    Console.WriteLine("Awaiting for agent connection...");
+    server.WaitForConnection();
+} catch
+{
+    Console.WriteLine("Something went wrong with connection.");
+    return;
+}
+Console.WriteLine("Connected successfully. Awaiting for data.");
+
+// Wait for data.
+// Process the data.
+// Print results.
+
+using var reader = new StreamReader(server);
+string? message = reader.ReadLine();
+
+if (message == null)
+    return;
+
+Console.WriteLine($"Received: {message}");
+
+Console.ReadKey();
